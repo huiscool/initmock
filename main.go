b86ac683f3f799e5b9d2d73cd5e5805a292aad91
huiscool <me@huis.cool>
2022-11-08 19:41:04 +0800

    implements doReplace

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"reflect"
 	"regexp"
 	"runtime"
+	"strconv"
+	"strings"
 	"syscall"
 	"unsafe"
 )
@@ -104,12 +106,80 @@ func skip(execName string, toSkips []string) {
 }
 
 func replace(execName string, toReplaces []string) {
-	panic("replace is not implemented")
+	srcToDst := parseAndCheckReplaceArgs(toReplaces)
+	exec := open(execName)
+	defer exec.file().Close()
+	for src, dst := range srcToDst {
+		doReplace(exec, src, dst)
+	}
+}
+
+// 1. invalidate src function.
+// 2. swap function pointer in src/dst inittask;
+func doReplace(exec goexec, srcRaw, dstRaw string) {
+	src := exec.getInitFunc(srcRaw)
+	dst := exec.getInitFunc(dstRaw)
+	srcTask := exec.getInitTask(src.pkgName)
+	dstTask := exec.getInitTask(dst.pkgName)
+	invalidateInitFunc(exec.file(), src)
+	getFnIdxInTask := func(t *initTask, f *initFunc) int {
+		for i, fptr := range t.infile.fns {
+			if fptr == uintptr(f.vmoffset) {
+				return i
+			}
+		}
+		Exitf("cannot find function %s in inittask", f.initFuncName.Raw())
+		return -1
+	}
+	si := getFnIdxInTask(srcTask, src)
+	di := getFnIdxInTask(dstTask, dst)
+	sptr := srcTask.infile.fns[si]
+	dptr := dstTask.infile.fns[di]
+	srcTask.infile.fns[si] = dptr
+	dstTask.infile.fns[di] = sptr
+	// write back
+	writeInitTaskAt(exec.file(), int(srcTask.fileOffset), srcTask.infile)
+	writeInitTaskAt(exec.file(), int(dstTask.fileOffset), dstTask.infile)
+}
+
+func parseAndCheckReplaceArgs(toReplaces []string) map[string]string {
+	var srcToDst = map[string]string{}
+	// further parse args
+	for _, item := range toReplaces {
+		if item == "" {
+			Exitf("empty replace argument")
+		}
+		funcNames := strings.Split(item, ":")
+		if len(funcNames) != 2 {
+			Exitf("invalid replace argument %s, should be like \"<source init function name>:<destination init function name>\"", item)
+		}
+		src, dst := funcNames[0], funcNames[1]
+		if another, ok := srcToDst[src]; ok {
+			Exitf("invalid replace argument %s, conflicts with %s", item, src+":"+another)
+		}
+		srcToDst[src] = dst
+	}
+	// check func form
+	for src, dst := range srcToDst {
+		initFuncNameFromRaw(src)
+		initFuncNameFromRaw(dst)
+	}
+	// for now we only support one step replace.
+	for _, dst := range srcToDst {
+		if _, ok := srcToDst[dst]; ok {
+			Exitf("invalid replace argument, cannot replace destination function %s", dst)
+		}
+	}
+	return srcToDst
 }
 
 //==============================================================================
 // helpers
 //==============================================================================
+
+func Exitf(format string, args ...interface{}) {
+	panic(fmt.Sprintf(format, args...))
+}
 
 func mayExitOn(err error, args ...interface{}) {
 	if err != nil {
@@ -131,7 +201,7 @@ func open(name string) (exec goexec) {
 	case "darwin/amd64":
 		return openMacho(name)
 	default:
-		mayExitOn(fmt.Errorf("unknown distribution %s", dist))
+		Exitf("unknown distribution %s", dist)
 		return nil
 	}
 }
@@ -152,14 +222,10 @@ func debug(format string, arg ...interface{}) {
 // abstract exec
 //==============================================================================
 
-const (
-	sectName = "__noptrdata"
-	segName  = "__DATA"
-)
-
 type goexec interface {
 	file() *os.File
 	getInitTask(pkgname string) *initTask
+	getInitFunc(funcname string) *initFunc
 }
 
 type symbol struct {
@@ -179,6 +245,25 @@ type initTask struct {
 	vmoffset   uint64
 	fileOffset uint64
 	infile     *initTaskInFile
+}
+
+type initFunc struct {
+	initFuncName
+	vmoffset   uint64
+	fileOffset uint64
+}
+
+type initFuncName struct {
+	pkgName string
+	index   int32
+}
+
+func (f initFuncName) Raw() string {
+	return fmt.Sprintf("%s.init.%d", f.pkgName, f.index)
+}
+
+func (f initFuncName) TaskName() string {
+	return taskNameFromRawFuncname(f.Raw())
 }
 
 type initTaskHeader struct {
@@ -278,12 +363,12 @@ func genInittask(
 
 	sym, ok := syms[symName]
 	if !ok {
-		mayExitOn(fmt.Errorf("cannot find %s in symbol table", symName))
+		Exitf("cannot find %s in symbol table", symName)
 	}
 	// get file offset from sections
 	sect, ok := sects[sectName]
 	if !ok {
-		mayExitOn(fmt.Errorf("cannot find data section info"))
+		Exitf("cannot find data section info")
 	}
 	foffset := sect.fileoffset + (sym.vmoffset - sect.vmoffset)
 	infile := readInitTaskAt(file, foffset)
@@ -294,6 +379,58 @@ func genInittask(
 		vmoffset:   sym.vmoffset,
 		fileOffset: foffset,
 		infile:     infile,
+	}
+}
+
+func genInitFunc(
+	symName string,
+	sectName string,
+	syms map[string]*symbol,
+	sects map[string]*sectionInfo,
+	file io.ReaderAt,
+) *initFunc {
+	sym, ok := syms[symName]
+	if !ok {
+		Exitf("cannot find %s in symbol table", symName)
+	}
+	// get file offset from sections
+	sect, ok := sects[sectName]
+	if !ok {
+		Exitf("cannot find %s section info", sectName)
+	}
+	foffset := sect.fileoffset + (sym.vmoffset - sect.vmoffset)
+	return &initFunc{
+		initFuncName: initFuncNameFromRaw(symName),
+		vmoffset:     sym.vmoffset,
+		fileOffset:   foffset,
+	}
+}
+
+func writeInitFuncAt(f io.WriterAt, fileOffset int, bin []byte) {
+	_, err := f.WriteAt(bin, int64(fileOffset))
+	mayExitOn(err, "cannot write init func")
+}
+
+func invalidateInitFunc(f io.WriterAt, initfunc *initFunc) {
+	var ret = []byte{0xC, 0x3} // x86 return instruction
+	writeInitFuncAt(f, int(initfunc.fileOffset), ret)
+}
+
+func taskNameFromRawFuncname(funcname string) string {
+	f := initFuncNameFromRaw(funcname)
+	return fmt.Sprintf("%s..inittask", f.pkgName)
+}
+
+func initFuncNameFromRaw(raw string) initFuncName {
+	submatch := regexp.MustCompile(`^(\S+)\.init\.(\d+)$`).FindStringSubmatch(raw)
+	if submatch == nil {
+		Exitf("failed to extract init func name %s", raw)
+	}
+	index, err := strconv.ParseInt(submatch[2], 10, 0)
+	mayExitOn(err, "failed to parse init func index")
+	return initFuncName{
+		pkgName: submatch[1],
+		index:   int32(index),
 	}
 }
 
@@ -364,6 +501,12 @@ func (m *machoExec) getInitTask(pkgName string) *initTask {
 	return genInittask(symName, sectName, m.syms, m.sects, m.f)
 }
 
+func (m *machoExec) getInitFunc(funcName string) *initFunc {
+	symName := funcName
+	sectName := "__text"
+	return genInitFunc(symName, sectName, m.syms, m.sects, m.f)
+}
+
 //==============================================================================
 // elf
 //==============================================================================
@@ -428,6 +571,10 @@ func (e *elfExec) getInitTask(pkgName string) *initTask {
 	symName := fmt.Sprintf("%s..inittask", pkgName)
 	sectName := ".noptrdata"
 	return genInittask(symName, sectName, e.syms, e.sects, e.f)
+}
+
+func (e *elfExec) getInitFunc(funcName string) *initFunc {
+	panic("not implemented")
 }
 
 //==============================================================================
@@ -496,4 +643,8 @@ func (p *peExec) getInitTask(pkgName string) *initTask {
 	symName := fmt.Sprintf("%s..inittask", pkgName)
 	sectName := ".data"
 	return genInittask(symName, sectName, p.syms, p.sects, p.f)
+}
+
+func (p *peExec) getInitFunc(funcName string) *initFunc {
+	panic("not implemented")
 }
